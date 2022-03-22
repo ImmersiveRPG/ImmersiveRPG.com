0fdf8b6b2d46bd1b9af5cdf455be7f78ba2d4a24
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2022-03-21 18:03:27 -0700

    Added PHP to get latest video from RSS feed

--- index.php
+++ index.php
@@ -1,4 +1,50 @@
 <!DOCTYPE html>
+
+<?php
+// Show all errors
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+function setup() {
+	$ids = [];
+
+	// Get the RSS feed for this Youtube channel
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, 'https://www.youtube.com/feeds/videos.xml?channel_id=UCfRXKGrGO9h6O0lAsSjbsCw');
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+	$data = curl_exec($ch);
+	$http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	curl_close($ch);
+	if ($http_status !== 200) {
+		return $ids;
+	}
+
+	// Parse the feed as XML
+	$feed = simplexml_load_string($data);
+	if ($feed === false) {
+		return $ids;
+	}
+
+	// Get the ids for all the videos
+	foreach ($feed->children() as $child) {
+		if ($child->getName() == "entry") {
+			foreach($child->children() as $sub_child) {
+				if ($sub_child->getName() == "id") {
+					$id = preg_replace('/^yt:video:/', '', $sub_child);
+					$ids[] = $id;
+				}
+			}
+		}
+	}
+
+	return $ids;
+}
+
+$ids = setup();
+?>
+
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
@@ -132,9 +178,14 @@
 			}
 		</style>
 		<h1>Let's make an Immersive RPG in Godot</h1>
+
 		<segment class="video">
 			<div>
+				<? if (count($ids) > 0) { ?>
+				<iframe id="youtube_video" src="https://www.youtube.com/embed/<?= $ids[0] ?>" frameborder="0" allowfullscreen></iframe>
+				<? } else { ?>
 				<iframe src="https://www.youtube.com/embed/?list=PLlnj-W0EhsbtCTalstIUDmV2ZKgFbNkth" frameborder="0" allowfullscreen></iframe>
+				<? } ?>
 			</div>
 		</segment>
 		<segment class="links">
